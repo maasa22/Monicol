3a30b415cbd487c094f56a1309f6c089cbdd9fbf
masaki <maa22sa@gmail.com>
2021-04-30 11:04:40 +0900

    enable users to add card

--- client/pages/helloworld_stripe.vue
+++ client/pages/helloworld_stripe.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <div id="card-element"></div>
+    <h2>Payment Methods</h2>
+    <details id="add-new-card">
+      <summary>Add new</summary>
+      <form id="payment-method-form" @submit.prevent="addNewCard">
+        <label>
+          氏名
+          <input type="text" name="name" required v-model="name" />
+        </label>
+        <fieldset>
+          <div id="card-element"></div>
+        </fieldset>
+        <div id="error-message" role="alert"></div>
+        <p><input type="submit" value="submit" /></p>
+      </form>
+    </details>
+    <hr />
     <div style="height:100px"></div>
     <div id="firebaseui-auth-container"></div>
     <v-btn @click="signout">sign out</v-btn>
@@ -12,9 +27,55 @@ import firebase from "~/plugins/firebase";
 
 export default {
   data() {
-    return {};
+    return {
+      name: "",
+      customerData: {},
+      currentUser: {},
+      cardElement: "",
+      test_cards: [
+        4242424242424242,
+        5555555555554444,
+        2223003122003222,
+        4000056655665556
+      ]
+    };
   },
   methods: {
+    // 書き直せばわんちゃん直るかな...？ form もあんまり使ったことないし。 コピペミスもありえるし。
+    async addNewCard(event) {
+      if (!event.target.reportValidity()) {
+        return;
+      }
+      const form = new FormData(event.target);
+      const cardholderName = form.get("name");
+
+      console.log(this.customerData.setup_secret);
+      console.log(this.cardElement);
+      console.log(cardholderName);
+      const setupIntent = await this.$stripe.confirmCardSetup(
+        this.customerData.setup_secret,
+        {
+          payment_method: {
+            card: this.cardElement,
+            billing_details: {
+              name: cardholderName
+            }
+          }
+        }
+      ); // should catch error
+      // firebase auth, forestore, stripe 顧客 全削除して、新メールアドレスでログイン、1つ目の支払い追加は上手くいく模様...
+      // server 側も作ってからエラー処理しても良いかも？
+      console.log(setupIntent);
+      console.log(setupIntent.setupIntent);
+      console.log(setupIntent.setupIntent.payment_method);
+      await firebase
+        .firestore()
+        .collection("stripe_customers")
+        .doc(this.currentUser.uid)
+        .collection("payment_methods")
+        //.add({ id: setupIntent.payment_method });
+        .add({ id: setupIntent.setupIntent.payment_method });
+    },
     signout() {
       firebase.auth().signOut();
     }
@@ -35,7 +96,7 @@ export default {
         }
       },
       signInFlow: "popup",
-      signInSuccessUrl: "/",
+      signInSuccessUrl: "/helloworld_stripe",
       signInOptions: [
         {
           provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
@@ -54,25 +115,31 @@ export default {
     firebase.auth().onAuthStateChanged(firebaseUser => {
       if (firebaseUser) {
         const currentUser = firebaseUser;
+        this.currentUser = currentUser;
         console.log(currentUser.uid);
+        // console.log(
+        //   firebase
+        //     .firestore()
+        //     .collection("stripe_customers")
+        //     .doc(currentUser.uid)
+        // );
         firebase
           .firestore()
           .collection("stripe_customers")
           .doc(currentUser.uid)
           .onSnapshot(snapshot => {
-            console.log(snapshot);
-            console.log(snapshot.data());
+            // console.log(snapshot);
+            // console.log(snapshot.data());
             if (snapshot.data()) {
-              console.log("hoge3");
-              let customerData = snapshot.data();
+              this.customerData = snapshot.data();
+              //   console.log("cusomerData-------------------", this.customerData);
               //   startDataListeners();
               //   document.getElementById("loader").style.display = "none";
               //   document.getElementById("content").style.display = "block";
             } else {
-              console.log("hoge4");
-              //   console.warn(
-              //     `No Stripe customer found in Firestore for user: ${currentUser.uid}`
-              //   );
+              console.warn(
+                `No Stripe customer found in Firestore for user: ${currentUser.uid}`
+              );
             }
           });
       } else {
@@ -80,12 +147,16 @@ export default {
         firebaseUI.start("#firebaseui-auth-container", firebaseUiConfig);
       }
     });
+    // show card-element
     if (this.$stripe) {
       const stripe = this.$stripe; // stripe is now available!!
       const elements = stripe.elements();
-      const card = elements.create("card", {});
+      const cardElement = elements.create("card", {
+        hidePostalCode: true
+      });
       // Add an instance of the card Element into the `card-element` <div>
-      card.mount("#card-element");
+      cardElement.mount("#card-element");
+      this.cardElement = cardElement;
     }
   }
 };
--- functions/index.js
+++ functions/index.js
@@ -33,6 +33,39 @@ exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
   return;
 });
 
+
+
+/**
+ * When adding the payment method ID on the client,
+ * this function is triggered to retrieve the payment method details.
+ */
+exports.addPaymentMethodDetails = functions.firestore
+  .document('/stripe_customers/{userId}/payment_methods/{pushId}')
+  .onCreate(async (snap, context) => {
+    try {
+      const paymentMethodId = snap.data().id;
+      const paymentMethod = await stripe.paymentMethods.retrieve(
+        paymentMethodId
+      );
+      await snap.ref.set(paymentMethod);
+      // Create a new SetupIntent so the customer can add a new method next time.
+      const intent = await stripe.setupIntents.create({
+        customer: `${paymentMethod.customer}`,
+      });
+      await snap.ref.parent.parent.set(
+        {
+          setup_secret: intent.client_secret,
+        },
+        { merge: true }
+      );
+      return;
+    } catch (error) {
+      await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+      await reportError(error, { user: context.params.userId });
+    }
+  });
+
+
 // const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
